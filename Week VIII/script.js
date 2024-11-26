96275ce35c12ffedb4468b03d2bcccadf4be3b32
Shqipe <shqipe@Shqipes-MacBook-Pro.local>
2024-11-26 14:04:14 +0100

    week viii samples

--- Week VIII/script.js
+++ Week VIII/script.js
@@ -4,16 +4,49 @@ import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHel
 
 const scene = new THREE.Scene();
 
-const ambientLight = new THREE.AmbientLight()
-ambientLight.color = new THREE.Color(0xffffff)
-ambientLight.intensity = 1
-scene.add(ambientLight)
+const ambientLight = new THREE.AmbientLight(0xffffff,1);
+// ambientLight.color = new THREE.Color(0xffffff);
+// ambientLight.intensity = 1;
+scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 50)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.9);
+directionalLight.position.set(1,0.25,0);
+scene.add(directionalLight);
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.8);
+scene.add(hemisphereLight);
+
+const pointLight = new THREE.PointLight(0xff9000, 1.5, 0, 2);
+pointLight.position.set(1, -0.5, 1);
+scene.add(pointLight);
+
+const reactAreaLight = new THREE.RectAreaLight(0x4e00ff, 6, 1, 1);
+reactAreaLight.position.set(-1.5, 0, 1.5);
+reactAreaLight.lookAt(new THREE.Vector3());
+scene.add(reactAreaLight);
+
+const spotLight = new THREE.SpotLight(0x78ff00, 4.5, 10, Math.PI * 0.1, 0.25, 1);
+spotLight.position.set(0,2,3);
+spotLight.target.position.x = -0.75;
+scene.add(spotLight);
+scene.add(spotLight.target);
+
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2);
+scene.add(hemisphereLightHelper);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2);
+scene.add(directionalLightHelper);
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
+scene.add(pointLightHelper);
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(spotLightHelper);
+
+const rectAreaLightHelper = new RectAreaLightHelper(reactAreaLight);
+scene.add(rectAreaLightHelper);
+
+
 
 const material = new THREE.MeshStandardMaterial();
 material.roughness = 0.4;
